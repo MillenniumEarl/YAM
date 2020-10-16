b0e9480c89b36ff58785a7298d415074e81b1638
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2020-10-16 12:00:27 +0200

    Preparing for multilanguage support

---
+++ app/src/scripts/localization.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// Core modules
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
+
+// Public modules from npm
+const i18next = require("i18next").default;
+const isDev = require("electron-is-dev");
+
+async function getTranslationResourcesFromDir(dirname) {
+    // Avoid callback and allow use of await
+    const readdir = util.promisify(fs.readdir);
+    const readfile = util.promisify(fs.readFile);
+
+    let resources = {};
+    let dir = path.resolve(dirname);
+    for (let filename of await readdir(path.resolve(dir))) {
+        // Read translation
+        let translationPath = path.join(dir, filename);
+        let json = await readfile(translationPath);
+
+        // Parse translation
+        let langTranslation = JSON.parse(json);
+        let langName = filename.split(".")[0];
+
+        // Add translation
+        resources[langName] = langTranslation;
+    }
+
+    return {
+        resources
+    };
+}
+
+module.exports.initLocalization = async function (resourcesPath) {
+    let res = await getTranslationResourcesFromDir(resourcesPath);
+    await i18next.init({
+        resources: res,
+        fallbackLng: isDev ? "dev" : "en",
+        debug: isDev
+    });
+}
+
+module.exports.getTranslation = function (key) {
+    return i18next.t(key);
+}
