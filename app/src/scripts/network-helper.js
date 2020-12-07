25a3528727576af318ca0a3a54e4e2fb4bd3ed35
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2020-12-07 13:44:50 +0100

    Added timeout

--- app/src/scripts/network-helper.js
+++ app/src/scripts/network-helper.js
@@ -8,18 +8,26 @@ const { promisify } = require("util");
 const DNS = "8.8.8.8";
 const PORT = 53;
 const alookupService = promisify(dns.lookupService);
+const atimeout = promisify(setTimeout);
 
 /**
  * @public
  * Check if the PC is connected to Internet.
  */
-module.exports.check = async function() {
+module.exports.checkConnection = async function(timeout = 30) {
     try {
+        // Set the timeout
+        atimeout(timeout * 1000).then(() => {
+            throw new Error("Timeout exceeded");
+        });
+
+        // Check DNS
         await alookupService(DNS, PORT);
         return true;
     }
     catch(err) {
         if(err.code === "ENOTFOUND") return false;
+        else if (err.message === "Timeout exceeded") return false;
         else throw err;
     }
 };
