b3606d0e5ac404429ebad6578db40e3c1dc28fe9
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2021-06-13 20:51:19 +0200

    Fix DeepSource JS-D009

--- app/src/scripts/io-operations.js
+++ app/src/scripts/io-operations.js
@@ -64,11 +64,12 @@ module.exports.deleteFolderRecursive = async function deleteFolderRecursive(dirp
 
             // Remove subdir
             const isDir = (await alstat(p)).isDirectory();
-            if (isDir) await exports.deleteFolderRecursive(p)
-                .catch(e => reportError(e, "30700", "exports.deleteFolderRecursive", "exports.deleteFolderRecursive", `Path: ${p}`));
-            // ...or remove single file
-            else await aunlink(p)
-                .catch(e => reportError(e, "30701", "aunlink", "exports.deleteFolderRecursive", `Path: ${p}`));
+            isDir 
+                ? await exports.deleteFolderRecursive(p)
+                    .catch(e => reportError(e, "30700", "exports.deleteFolderRecursive", "exports.deleteFolderRecursive", `Path: ${p}`))
+                // ...or remove single file
+                : await aunlink(p)
+                    .catch(e => reportError(e, "30701", "aunlink", "exports.deleteFolderRecursive", `Path: ${p}`));
         }
 
         // Remove main dir
