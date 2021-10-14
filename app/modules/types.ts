8c214988849937d9bee76dab7bec433ece42b57c
Millennium Earl <22562005+MillenniumEarl@users.noreply.github.com>
2021-10-14 21:16:34 +0200

    Add types.ts

---
+++ app/modules/types.ts
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 MillenniumEarl
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export type TLoggerCategory = "app.main" | "app.renderer" | "modules.f95";
