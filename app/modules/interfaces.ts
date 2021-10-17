60d685d528806201a4e82d9b9b4f08f3574b34c3
Millennium Earl <22562005+MillenniumEarl@users.noreply.github.com>
2021-10-17 20:12:36 +0200

    Add interfaces

--- app/modules/interfaces.ts
+++ app/modules/interfaces.ts
@@ -1,5 +1,63 @@
 // Copyright (c) 2021 MillenniumEarl
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+// Public modules from npm
+import { BrowserWindow } from "electron";
+
+// Local modules
+import { TCloseWindowCallbackNull, TCloseWindowCallbackRest } from "./types";
+
+/**
+ * Options used for creating a new `BrowserWindow`.
+ */
+export interface IWindowOptions {
+  /**
+   * Unique name of the window.
+   */
+  name: string;
+  /**
+   * Default size of the window
+   */
+  size: { width: number; height: number };
+  /**
+   * Minimum size of the window.
+   */
+  minSize?: { width: number; height: number };
+  /**
+   * Maximum size of the window.
+   */
+  maxSize?: { width: number; height: number };
+  /**
+   * Set if the window has a non-Chrome contourn.
+   */
+  hasFrame?: boolean;
+  /**
+   * Parent window for the new window.
+   *
+   * If set, the new window will behave like a modal dialog.
+   */
+  parent?: BrowserWindow | undefined;
+  /**
+   * Path to the preload script.
+   */
+  preloadPath: string;
+  /**
+   * Dictionary of elements to pass to the window as arguments.
+   */
+  args?: Record<string, any>;
+  /**
+   * Callback executed when the window is closed.
+   * It receives a return value from the window as the only parameter
+   */
+  onclose?: TCloseWindowCallbackRest | TCloseWindowCallbackNull;
+}
+
+export interface IWindowData {
+  window: BrowserWindow;
+  /**
+   * Options used when creating this window.
+   */
+  options: IWindowOptions;
+}
