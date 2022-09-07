b24b7bd7fa1a2531853db95ee48a6b5efc96e36a
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2022-09-07 20:21:23 +0200

    Clean code

--- packages/renderer/src/components/DataFilter.vue
+++ packages/renderer/src/components/DataFilter.vue
@@ -6,11 +6,13 @@
 -->
 
 <template>
-  <!-- We need a 4 columns grid with a template TEXT:COMPONENT -->
+  <!-- We need a 2 columns grid with a template | TEXT: COMPONENT | TEXT: COMPONENT | -->
   <div class="grid grid-flow-dense grid-cols-2 sm:grid-cols-1 gap-y-3 gap-x-5">
     <!-- Game title TextBox -->
     <div class="flex items-center gap-x-2">
-      Title
+      <span>
+        Title
+      </span>
       <text-box
         class="grow"
         placeholder="Title"
@@ -18,12 +20,16 @@
     </div>
     <!-- Last Update selection -->
     <div class="flex items-center gap-x-2">
-      Last Update
+      <span>
+        Last Update
+      </span>
       <date-time-picker />
     </div>
     <!-- Author selection -->
     <div class="flex items-center gap-x-2">
-      Author
+      <span>
+        Author
+      </span>
       <drop-down-text-box
         class="grow"
         placeholder="Author"
@@ -32,7 +38,9 @@
     </div>
     <!-- "Only user completed" game checkboxes -->
     <div class="flex items-center gap-x-4">
-      Only user completed:
+      <span>
+        Only user completed:
+      </span>
       <input
         id="chk-user-completed"
         class="checkbox"
@@ -41,10 +49,11 @@
     </div>
     <!-- Automatic generation of game statuses checkboxes -->
     <div class="flex items-center gap-x-4 col-span-2">
-      Status:
-      <!-- @todo Sobstitute with data from F95API -->
+      <span>
+        Status:
+      </span>
       <div
-        v-for="status in ['Completed', 'OnHold', 'Ongoing', 'Abandoned']"
+        v-for="status in gamestatus"
         :key="status"
       >
         <input
@@ -62,7 +71,9 @@
     </div>
     <!-- Tag selection -->
     <div class="flex items-center gap-x-2">
-      Tags
+      <span>
+        Tags
+      </span>
       <drop-down-text-box
         class="grow"
         placeholder="Game Tags"
@@ -88,33 +99,52 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import TextBox from "./TextBox.vue"; 
-import DropDownTextBox from "./DropDownTextBox.vue";
-import DateTimePicker from "./DateTimePicker.vue"; 
+import TextBox from "./base/TextBox.vue"; 
+import DropDownTextBox from "./base/DropDownTextBox.vue";
+import DateTimePicker from "./base/DateTimePicker.vue"; 
 
 export default defineComponent({
     name: "DataFilter",
     components: { DropDownTextBox, TextBox, DateTimePicker },
     data() {
       return {
+        /**
+         * List of game'authors from database.
+         * @todo Substitute with data from database
+         */
         authorlist: ["Autore 1", "Autore 2", "Autore del cazzo che mi frega", "Autore 4", "Autore 5", "Autore 6"],
+        /**
+         * List of game statuses from F95 API.
+         * @todo Substitute with data from F95API
+         */
+        gamestatus: ["Completed", "OnHold", "Ongoing", "Abandoned"],
+        /**
+         * List of game tags from F95 API.
+         * @todo Substitute with data from F95API
+         */
         taglist: ["Incest", "Slave", "Anal", "NTR", "Cheating", "Swinging", "Female domination", "Vaginal", "soooooooooLooooooong Taggggggg"],
+        /**
+         * Active tags selected by the user.
+         */
         tags: [] as string[]
       };
     },
     methods: {
+      /**
+       * Add an available game tag to the active list and remove it from `taglist`.
+       */
       addTag(tag: string) {
         this.tags.push(tag);
         this.tags = this.tags.sort();
         this.taglist = this.taglist.filter((tag) => !this.tags.includes(tag));
       },
+      /**
+       * Remove a tag from the active list and put it in `taglist`.
+       */
       removeTagButton(e: Event) {
         const TAG = (e.target as HTMLElement).innerHTML;
         this.tags = this.tags.filter((tag) => tag != TAG);
         this.taglist.push(TAG);
-      },
-      message(mex: string) {
-        console.log(mex);
       }
     }
 });
