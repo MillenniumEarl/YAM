6509a16e7639b103a7e842e232f9ec865c3fc16a
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2022-09-08 09:46:20 +0200

    Add query event

--- packages/renderer/src/components/GameTableQuery.vue
+++ packages/renderer/src/components/GameTableQuery.vue
@@ -1,15 +1,53 @@
 <template>
-  <div class="flex flex-nowrap">
-    <search-bar style="margin: 2px" />
+  <div class="flex flex-nowrap items-center gap-x-2 m-2">
+    <search-bar
+      class="grow"
+      :disabled="showAdvancedSearch"
+      @input-changed="query.title = $event"
+    />
+    <button
+      class="btn-icon bg-blue-500 hover:bg-blue-600 focus:ring-blue-500"
+      @click="showAdvancedSearch = !showAdvancedSearch"
+    >
+      <i-ion-funnel class="w-5 h-5 mr-1" />
+      <span>Advanced Search</span>
+    </button>
   </div>
+
+  <!-- Let's put it under the flex DIV so that we can hide it -->
+  <data-filter
+    v-if="showAdvancedSearch"
+    class="m-2.5"
+    @query-update="query = $event"
+  />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import SearchBar from "./SearchBar.vue";
+import SearchBar from "./base/SearchBar.vue";
+import DataFilter from "./DataFilter.vue";
+import type { IQuery } from "types/interfaces-vue";
 
 export default defineComponent({
-    name: "GameTableQuery",
-    components: { SearchBar }
+  name: "GameTableQuery",
+  components: { SearchBar, DataFilter },
+  emits: ["query-update"],
+  data() {
+    return {
+      /**
+       * Switch used to show/display the advance search panel.
+       */
+      showAdvancedSearch: false,
+      query: {} as IQuery
+    };
+  },
+  watch: {
+    query: {
+      handler() {
+        this.$emit("query-update", this.query);
+      },
+      deep: true
+    }
+  }
 });
 </script>
